6e0e7294f6fc785f6212ccb302d97057f1eb0fe7
Ignacy Tadeusz Kluszczyński <i.kluszczynski@stud.elka.pw.edu.pl>
2022-02-01 18:43:41 +0100

    added chart labels, added tests for different temperatures and different tabu sizes

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ def main():
     print("Annealing")
     show_sample_functions()
     display_visual_results()
-    test_efficiency(rosenbrock_f, func_name="rosenbrock", show_chart=True, logging=True)
-    test_efficiency(bird_f, func_name="rbird function", show_chart=True, logging=True)
-    test_efficiency(shubert_f, func_name="shubert function", show_chart=True, logging=True)
-    test_efficiency(cec_func, func_name="cec_func")
+
+    test_efficiency_for_temp_all()
+    test_efficiency_for_tabu_all()
+
+    # run_old_tests()
+
 
 
 main()
--- tests.py
+++ tests.py
@@ -6,9 +6,11 @@ import annealing_with_tabu as at
 import numpy as np
 from functions import *
 
-def show_chart(x_vals, y_vals, color='r', title="chart"):
-    plt.plot(x_vals, y_vals, color=color, marker='.', label="sym")
+def show_chart(x_vals, y_vals, color='r', title="chart", label="projection of convergence patch in 2d", x_label="x values", y_label="y values"):
+    plt.plot(x_vals, y_vals, color=color, marker='.', label=label)
     plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.tight_layout()
     plt.legend()
     plt.grid()
@@ -45,11 +47,11 @@ def print_data(points, q_val):
         print(f"row {row_num} : {label+row} : q {q}")
         row_num += 1
 
-def results(func, cooling_func, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data):
+def results(func, cooling_func, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data, func_name=""):
     alg = sa.SimulatedAnnealing(func, x_dim)
     points, q_val = alg.run_algorithm(init_min, init_max, max_iter, cooling_func)
     if show_data:
-        description = "Simulated Annealing"
+        description = "Simulated Annealing " + func_name
         print(description)
         print_data(points, q_val)
     if plotting:
@@ -61,7 +63,7 @@ def results(func, cooling_func, x_dim, t_0, param, init_min, init_max, max_iter,
     alg2 = at.AnnealingWithTabu(func, x_dim)
     points2, q_val2 = alg2.run_algorithm(init_min, init_max, max_iter, tabu_size, cooling_func)
     if show_data:
-        description2 = "Simulated Annealing with tabu"
+        description2 = "Simulated Annealing with tabu " + func_name
         print(description2)
         print_data(points2, q_val2)
     if plotting:
@@ -76,23 +78,23 @@ def results(func, cooling_func, x_dim, t_0, param, init_min, init_max, max_iter,
                             
 
 
-def run_all(func, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data, logging=False):
+def run_all(func, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data, logging=False, func_name=""):
     if logging:
         print(1)
     col = cs.StandardCoolingSchedule(t_0, param)
-    optimum_result1 = results(func, col.standard_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data)
+    optimum_result1 = results(func, col.standard_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data, func_name=func_name+" StandardCooling")
     if logging:
         print(2)
     col = cs.LinearCoolingSchedule(t_0, param)
-    optimum_result2 = results(func, col.linear_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data)
+    optimum_result2 = results(func, col.linear_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data, func_name=func_name+" Linear Cooling")
     if logging:
         print(3)
     col = cs.GeometricCoolingSchedule(t_0, param)
-    optimum_result3 = results(func, col.geometric_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data)
+    optimum_result3 = results(func, col.geometric_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data, func_name=func_name+" Geometric Cooling")
     if logging:
         print(4)
     col = cs.LogarithmicCoolingSchedule(t_0, param)
-    optimum_result4 = results(func, col.logarithmic_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data)
+    optimum_result4 = results(func, col.logarithmic_cooling, x_dim, t_0, param, init_min, init_max, max_iter, tabu_size, plotting, show_data, func_name=func_name+" Logharytmic Cooling")
     optimum_results = [optimum_result1, optimum_result2, optimum_result3, optimum_result4]
     return optimum_results
 
@@ -105,14 +107,14 @@ def show_sample_functions():
     dim3_chart(bird_f, 'bird function')
 
 def display_visual_results():
-    run_all(func0, x_dim=1, t_0=2.0, param=0.1, init_min=-5, init_max=5, max_iter=100, tabu_size=2, plotting=True, show_data=True)
-    run_all(func1, x_dim=1, t_0=2.0, param=0.1, init_min=-5, init_max=5, max_iter=100, tabu_size=2, plotting=True, show_data=True)
-    run_all(func2, x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=2, plotting=True, show_data=True)
-    run_all(rosenbrock_f, x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=2, plotting=True, show_data=True)
-    run_all(shubert_f, x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=2, plotting=True, show_data=True)
-    run_all(bird_f, x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=2, plotting=True, show_data=True)
-    run_all(cec_func, x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=100, tabu_size=2, plotting=True, show_data=True)
-    run_all(cec_func, x_dim=10, t_0=10.0, param=0.1, init_min=-10, init_max=10, max_iter=100, tabu_size=2, plotting=False, show_data=True)
+    run_all(func0, func_name="func0", x_dim=1, t_0=2.0, param=0.1, init_min=-5, init_max=5, max_iter=100, tabu_size=2, plotting=True, show_data=True)
+    run_all(func1, func_name="func1", x_dim=1, t_0=2.0, param=0.1, init_min=-5, init_max=5, max_iter=100, tabu_size=2, plotting=True, show_data=True)
+    run_all(func2, func_name="func2", x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=2, plotting=True, show_data=True)
+    run_all(rosenbrock_f, func_name="rosenbrock_f", x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=2, plotting=True, show_data=True)
+    run_all(shubert_f,func_name="shubert_f", x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=2, plotting=True, show_data=True)
+    run_all(bird_f, func_name="bird_f", x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=1000, tabu_size=20, plotting=True, show_data=True)
+    run_all(cec_func, func_name="cec_func", x_dim=2, t_0=2.0, param=0.1, init_min=-100, init_max=100, max_iter=100, tabu_size=2, plotting=True, show_data=True)
+    run_all(cec_func, func_name="cec_func", x_dim=10, t_0=10.0, param=0.1, init_min=-10, init_max=10, max_iter=100, tabu_size=2, plotting=False, show_data=True)
 
 def get_test_data(func):
     t_0_data = [1.0, 2.0, 10.0, 50.0, 100.0, 200.0]
@@ -122,8 +124,29 @@ def get_test_data(func):
     functions = [func]
     return [t_0_data, param_data, init_data, tabu_size_data, functions]
 
-def get_quality_results(func, get_test_data, logging, max_iter=100):
-    t_0_data, param_data, init_data, tabu_size_data, functions = get_test_data(func)
+def get_test_data_temp(func, temp):
+    t_0_data = temp
+    param_data = [0.1]
+    init_data = [100]
+    tabu_size_data = [5]
+    functions = [func]
+    return [t_0_data, param_data, init_data, tabu_size_data, functions]
+
+def get_test_data_tabu(func, tabu):
+    t_0_data = [1.0]
+    param_data = [0.1]
+    init_data = [100]
+    tabu_size_data = tabu
+    functions = [func]
+    return [t_0_data, param_data, init_data, tabu_size_data, functions]
+
+def get_quality_results(func, get_test_data, logging, max_iter=100, temp=None, tabu=None):
+    if not temp and not tabu:
+        t_0_data, param_data, init_data, tabu_size_data, functions = get_test_data(func)
+    elif not tabu:
+        t_0_data, param_data, init_data, tabu_size_data, functions = get_test_data(func, temp)
+    else:
+        t_0_data, param_data, init_data, tabu_size_data, functions = get_test_data(func, tabu)
     standard_cool_res = [[], []]
     linear_cool_res = [[], []]
     geometric_cool_res = [[], []]
@@ -152,8 +175,9 @@ def get_quality_results(func, get_test_data, logging, max_iter=100):
 
     return [standard_cool_res, linear_cool_res, geometric_cool_res, logarithmic_cool_res]
 
-def show_chart_all(y1, y2, y3, y4, y5, y6, y7, y8, title="chart"):
-    x = [i for i in range(1, len(y1) + 1)]
+def show_chart_all(y1, y2, y3, y4, y5, y6, y7, y8, title="chart", x=None):
+    if x == None:
+        x = [i for i in range(1, len(y1) + 1)]
     plt.scatter(x, y1, color='red', marker='.', label="no tabu - standard cool")
     plt.scatter(x, y2, color='orange', marker='.', label="tabu - standard cool")
     plt.scatter(x, y3, color='blue', marker='.', label="no tabu - linear cool")
@@ -167,6 +191,18 @@ def show_chart_all(y1, y2, y3, y4, y5, y6, y7, y8, title="chart"):
     plt.legend()
     plt.grid()
     plt.show()
+
+def show_chart_all_sep(ys, title="chart", x=None, x_label="temperature"):
+    if x == None:
+        x = [i for i in range(1, len(ys[0]) + 1)]
+    colors = ['red', 'orange', 'blue', 'cyan', 'forestgreen', 'limegreen', 'violet', 'purple']
+    labels = ["no tabu - standard cool", "tabu - standard cool", "no tabu - linear cool", "tabu - linear cool",
+                "no tabu - geometric cool", "tabu - geometric cool", "no tabu - logarytmic cool", "tabu - logarytmic cool"
+                    ]
+
+    for y, color, label in zip(ys, colors, labels):
+        show_chart(x, y, color=color, title="best solution found", label=label, x_label=x_label, y_label="q values")
+
 
 def best_efficiency(st, li, ge, lo):
     evaluation = [0,0,0,0,0,0,0,0]
@@ -200,3 +236,84 @@ def test_efficiency(func, func_name, show_chart=True, logging=True):
         show_chart_all(st[0], st[1], li[0], li[1], ge[0], ge[1], lo[0], lo[1], func_name)
     ef = best_efficiency(st, li, ge, lo)
     return percentage(ef)
+
+def test_efficiency_for_temp(func, func_name, show_chart=True, logging=True, temps=None, write_to_file=True):
+    st, li, ge, lo = get_quality_results(func, get_test_data_temp, logging=logging, temp=temps)
+    results = [st[0], st[1], li[0], li[1], ge[0], ge[1], lo[0], lo[1]]
+    for i in range(0, 100):
+        st, li, ge, lo = get_quality_results(func, get_test_data_temp, logging=logging, temp=temps)
+        new_results = [st[0], st[1], li[0], li[1], ge[0], ge[1], lo[0], lo[1]]
+        for j in range(0, len(results)):
+            for k in range(0, len(results[0])):
+                results[j][k] = min(results[j][k], new_results[j][k])
+
+    if show_chart:
+        show_chart_all_sep(results, func_name, x=temps)
+
+    if write_to_file:
+        f = open(f"{func_name}_res.txt", "a")
+        f.write(f"temperature : shema_nums \n")
+        f.write(f"T : 1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 \n")
+        print(results)
+        for i in range(0, len(results[0])):
+            f.write(f"t:{temps[i]} ")
+        f.write("\n")
+        for i in range(0, len(results)):
+            for j in range(0, len(results[0])):
+                f.write(f"{results[i][j]} ")
+            f.write(f"\n")
+        
+        f.write("------------------------------------------------------")
+        f.write('\n')
+        f.close()
+
+    return results
+
+def test_efficiency_for_tabu(func, func_name, show_chart=True, logging=True, tabus=None, write_to_file=True):
+    st, li, ge, lo = get_quality_results(func, get_test_data_tabu, logging=logging, tabu=tabus)
+    results = [st[0], st[1], li[0], li[1], ge[0], ge[1], lo[0], lo[1]]
+    for i in range(0, 100):
+        st, li, ge, lo = get_quality_results(func, get_test_data_tabu, logging=logging, tabu=tabus)
+        new_results = [st[0], st[1], li[0], li[1], ge[0], ge[1], lo[0], lo[1]]
+        for j in range(0, len(results)):
+            for k in range(0, len(results[0])):
+                results[j][k] = min(results[j][k], new_results[j][k])
+
+    if show_chart:
+        show_chart_all_sep(results, func_name, x=tabus, x_label="tabu size")
+
+    if write_to_file:
+        f = open(f"{func_name}_res_tabu.txt", "a")
+        f.write(f"tabu : shema_nums \n")
+        print(results)
+        for i in range(0, len(results[0])):
+            f.write(f"t:{tabus[i]} ")
+        f.write("\n")
+        for i in range(0, len(results)):
+            for j in range(0, len(results[0])):
+                f.write(f"{results[i][j]} ")
+            f.write(f"\n")
+        
+        f.write("------------------------------------------------------")
+        f.write('\n')
+        f.close()
+
+    return results
+
+def test_efficiency_for_temp_all():
+    test_efficiency_for_temp(rosenbrock_f, func_name="rosenbrock", temps=[i for i in range(1, 200, 5)], show_chart=True, logging=True, write_to_file=True)
+    test_efficiency_for_temp(shubert_f, func_name="shubert", temps=[i for i in range(1, 200, 5)], show_chart=True, logging=True, write_to_file=True)
+    test_efficiency_for_temp(bird_f, func_name="bird", temps=[i for i in range(1, 200, 5)], show_chart=True, logging=True, write_to_file=True)
+    test_efficiency_for_temp(cec_func, func_name="cec", temps=[i for i in range(1, 200, 15)], show_chart=True, logging=True, write_to_file=True)
+ 
+def test_efficiency_for_tabu_all():
+    test_efficiency_for_tabu(rosenbrock_f, func_name="rosenbrock", tabus=[i for i in range(1, 100, 4)], show_chart=True, logging=True, write_to_file=True)
+    test_efficiency_for_tabu(shubert_f, func_name="shubert", tabus=[i for i in range(1, 100, 4)], show_chart=True, logging=True, write_to_file=True)
+    test_efficiency_for_tabu(bird_f, func_name="bird", tabus=[i for i in range(1, 100, 4)], show_chart=True, logging=True, write_to_file=True)
+    test_efficiency_for_tabu(cec_func, func_name="cec", tabus=[2, 5, 10, 20, 30, 40, 50, 75, 100], show_chart=True, logging=True, write_to_file=True)
+
+def run_old_tests():
+    test_efficiency(rosenbrock_f, func_name="rosenbrock function", show_chart=True, logging=True)
+    test_efficiency(bird_f, func_name="rbird function", show_chart=True, logging=True)
+    test_efficiency(shubert_f, func_name="shubert function", show_chart=True, logging=True)
+    test_efficiency(cec_func, func_name="cec_func")
